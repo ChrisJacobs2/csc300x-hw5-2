653178091cbb5a1149e15560e0e51662dbd2ee0a
ChrisJacobs2 <cmjacobs@uncg.edu>
2024-03-17 23:26:23 -0400

    completed endpoint iii

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 
 // das express und multer
 const express = require('express');
-const multer = require('multer');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
 const port = 3000;
 
 // das variables :^O
@@ -33,6 +34,9 @@ let lameJoke = [
     }
 ];
 
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
 app.get('/', (req, res) => {
     res.send('Hello World!');
   });
@@ -41,7 +45,46 @@ app.get('/jokebook/categories', (req, res) => {
     res.json(categories);
 });
   
-  app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-  });
+app.get('/jokebook/joke/:category', (req, res) => {
+    let category = req.params.category;
+    let limit = req.query.limit;
 
+    let jokes;
+
+    if (category === 'funnyJoke') {
+        jokes = funnyJoke;
+    } else if (category === 'lameJoke') {
+        jokes = lameJoke;
+    } else {
+        return res.status(404).json({error: `No category listed for ${category}`});
+    }
+
+    if (limit) {
+        limit = parseInt(limit);
+        jokes = jokes.slice(0, limit);
+    }
+
+    res.json(jokes);
+});
+
+app.post('/jokebook/joke/new', (req, res) => {
+    let category = req.body.category;
+    let joke = req.body.joke;
+    let response = req.body.response;
+
+    if (!category || !joke || !response || (category !== 'funnyJoke' && category !== 'lameJoke')) {
+        return res.status(400).json({error: 'Invalid or insufficient user input'});
+    }
+
+    let newJoke = {joke: joke, response: response};
+
+    if (category === 'funnyJoke') {
+        funnyJoke.push(newJoke);
+        res.json(funnyJoke);
+        console.log('funny joke added');
+    } else {    // the category is lameJoke
+        lameJoke.push(newJoke);
+        res.json(lameJoke);
+        console.log('lame joke added');
+    }
+});
